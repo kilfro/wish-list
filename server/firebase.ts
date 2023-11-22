e3395962ec35cee2c9236743a3c2e2d74bdbfed5
kirillfrolov <frolov.cyrill@gmail.com>
2023-11-23 00:35:06 +0300

    Google credentials use env

--- server/firebase.ts
+++ server/firebase.ts
@@ -1,10 +1,9 @@
 import admin from 'firebase-admin'
-
-const serviceAccount = require('./admin.json')
+import { applicationDefault } from 'firebase-admin/app'
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://wish-list-861de.firebaseio.com"',
+    credential: applicationDefault(),
+    databaseURL: 'https://wish-list-861de.firebaseio.com',
 })
 
 export const DB: admin.firestore.Firestore = admin.firestore()
